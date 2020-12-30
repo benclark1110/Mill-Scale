b781156ce38b9d49b19b47522fe4fca182017f42
Ben Clark <ben.clark@parivedasolutions.com>
2020-12-29 20:54:16 -0500

    adding third rotating image

--- assets/application.js
+++ assets/application.js
@@ -33,20 +33,25 @@ function startTimer() {
 
 var images = [], x = -1;
 images[0] = "//cdn.shopify.com/s/files/1/0467/0049/5015/t/5/assets/Hero_2@2x.png?v=16895830379331391678";
-images[1] = "//cdn.shopify.com/s/files/1/0467/0049/5015/t/5/assets/home-image.png?v=54031112577287740";
+images[1] = "//cdn.shopify.com/s/files/1/0467/0049/5015/t/5/assets/MaskGroup.png?v=5047302691914966501";
+images[2] = "//cdn.shopify.com/s/files/1/0467/0049/5015/t/5/assets/home-image.png?v=54031112577287740";    
 
 var text = [], y = -1;
 text[0] = "FORM MEETS FUNCTION";
-text[1] = "KEEP THE FIRES BURNING";
+text[1] = "WHY WE DO IT";
+text[2] = "KEEP THE FIRES BURNING";
 
 var paragraph = [], z = -1;
-paragraph[0] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. my nibh euismod tincidunt ut laoreet dolore magna.";
-paragraph[1] = "We build 500 and 1000 gallon offset smokers for competition, commercial, and residential use. Each one is built to suit the pitmaster’s needs. Ready to take your barbecue to the next level?";
+paragraph[0] = "We are now offering carbon steel fire and grill tools in collaboration with Iron Grove Tool Company. Each item is hand-forged in Dripping Springs, Texas.";
+paragraph[1] = "Matt and Caleb Johnson have been working together their entire lives. Their requires discipline, adaptability, and time. And at the end of the day, you’ve just got to have the heat right.";
+paragraph[2] = "We build 500 and 1000 gallon offset smokers for competition, commercial, and residential use. Each one is built to suit the pitmaster’s needs. Ready to take your barbecue to the next level?";
 
 var link = [], a = -1;
 link[0] = "https://mill-scale.myshopify.com/collections/fire-tools";
-link[1] = "https://mill-scale.myshopify.com/collections/cookers/products/offset-smoker";
+link[1] = "https://mill-scale.myshopify.com/pages/our-story";
+link[2] = "https://mill-scale.myshopify.com/collections/cookers/products/offset-smoker";
 
 var sideText = [], b = -1;
 sideText[0] = "Handcrafted Fire Tools";
-sideText[1] = "Custom Offset Smokers";
+sideText[1] = "Our Story";
+sideText[2] = "Custom Offset Smokers";
